7b81087f1c07424e81197536296f870e93afd6a4
WangWilly <s89667654@gmail.com>
2023-01-04 10:56:22 +0800

    feat: add 'p2p' functionality

---
+++ src/p2p.rs
@@ -0,0 +1,47 @@
+use super::{App, Block};
+use libp2p::{
+    floodsub::{Floodsub, FloodsubEvent, Topic},
+    identity,
+    mdns::{Mdns, MdnsEvent},
+    swarm::{NetworkBehaviourEventProcess, Swarm},
+    NetworkBehaviour, PeerId,
+};
+use log::{error, info};
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
+use tokio::sync::mpsc;
+
+pub static KEYS: Lazy = Lazy::new(identity::Keypair::generate_ed25519);
+pub static PEER_ID: Lazy = Lazy::new(|| PeerId::from(KEYS.public()));
+pub static CHAIN_TOPIC: Lazy = Lazy::new(|| Topic::new("chains"));
+pub static BLOCK_TOPIC: Lazy = Lazy::new(|| Topic::new("blocks"));
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ChainResponse {
+    pub blocks: Vec,
+    pub receiver: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LocalChainRequest {
+    pub from_peer_id: String,
+}
+
+pub enum EventType {
+    LocalChainResponse(ChainResponse),
+    Input(String),
+    Init,
+}
+
+#[derive(NetworkBehaviour)]
+pub struct AppBehaviour {
+    pub floodsub: Floodsub,
+    pub mdns: Mdns,
+    #[behaviour(ignore)]
+    pub response_sender: mpsc::UnboundedSender,
+    #[behaviour(ignore)]
+    pub init_sender: mpsc::UnboundedSender,
+    #[behaviour(ignore)]
+    pub app: App,
+}
